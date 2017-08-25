97c29443038aa76fee060b4bf013e2234de34f06
Rio <rnose512@gmail.com>
2017-08-24 21:56:13 -0700

    Add some styling

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ get '/recipes/search' do
 	url = "http://api.yummly.com/v1/api/recipes"
 	app_id = ENV['RECIPE_API_APP_ID']
 	app_key = ENV['RECIPE_API_APP_KEY']
-	q = params[:searchwords].split(",")
+	q = params[:searchwords].split(", ")
 	@recipes = HTTParty.get(url, { query: { _app_id: app_id, _app_key: app_key, q: q } })
-	p @recipes
+	p @recipes[1]
 end
